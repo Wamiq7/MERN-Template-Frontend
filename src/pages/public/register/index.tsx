a77e91406692d1321d61c88034e7bb1a5e979736
Hamza Zaheer <mhamzazaheer1@gmail.com>
2025-01-23 21:38:13 +0500

    new seo system

--- src/pages/public/register/index.tsx
+++ src/pages/public/register/index.tsx
@@ -1,25 +1,10 @@
+import { Head } from '@/lib/react-helmet-async/Head';
 import RegisterPage from './register.page';
 
 const index = () => {
   return (
     <>
-      <title>Register</title>
-      <meta name="register-description" content="This is a register page of the application" />
-
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="article" />
-      <meta property="og:url" content="https://on.foundation" />
-      <meta property="og:title" content="Foundation" />
-      <meta property="og:description" content="A revolutionary new social platform. Own your data. Get rewarded." />
-      <meta property="og:image" content="https://foundation-seo.s3.amazonaws.com/seo-logo-v2.png" />
-
-      {/* <!-- Twitter --> */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://on.foundation" />
-      <meta name="twitter:title" content="Foundation" />
-      <meta name="twitter:description" content="A revolutionary new social platform. Own your data. Get rewarded." />
-      <meta name="twitter:image" content="https://foundation-seo.s3.amazonaws.com/seo-logo-v2.png" />
-
+      <Head title="Register" description="This is a register page of the application" />
       <RegisterPage />
     </>
   );
